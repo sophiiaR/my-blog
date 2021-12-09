8ed85da630c54b7cdf35d54b1b0a1bb778132f17
Sofia <sraissiguier@gmail.com>
2021-12-09 11:36:01 -0300

    Create admin categories views

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,57 @@
+@extends('adminlte::page')
+
+@section('title', 'My Blog')
+
+@section('content_header')
+    <h1>Category list</h1>
+@stop
+
+@section('content')
+    @if (session('info'))
+    <div class="alert alert-success">
+        <strong>{{session('info')}}</strong>
+    </div>
+    @endif
+    <div class="card">
+        <div class="card-header">
+            <a href="{{route('admin.categories.create')}}" class="btn btn-secondary">Add Category</a>
+        </div>
+        <div class="card-body">
+            <table class="table table-stripped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td>{{$category->id}}</td>
+                            <td>{{$category->name}}</td>
+                            <td width="10px">
+                                <a href="{{route('admin.categories.edit', $category)}}" class="btn btn-primary btn-sm">Edit</a>
+                            </td>
+                            <td width="10px">
+                                <form action="{{route('admin.categories.destroy', $category)}}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@stop
+
+{{-- @section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop --}}
