4dcc3dd32b4d1b6e17d228db434944cc0fbc29b2
Sofia <sraissiguier@gmail.com>
2021-12-13 09:17:49 -0300

    Change logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -47,10 +47,11 @@ return [
 
     'logo' => '<b>My Blog</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    //'logo_img' => 'public/logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'logo',
 
     /*
     |--------------------------------------------------------------------------
